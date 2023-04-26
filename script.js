da92fc1ab9fc7faf051f6c57c915a3d6536b0cda
Konstantinwebworks <konstantinwebworks@gmail.com>
2023-04-26 19:01:16 +0300

    trash

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+const popup = document.querySelector(".popup");
+const trashBtn = document.querySelector(".trash");
+const closePopupBtn = document.querySelector(".close-popup");
+const cards = document.querySelectorAll(".cards");
+const trashOut = document.querySelector(".trash-out");
+
+trashBtn.addEventListener("click", function () {
+  popup.style.display = "block";
+});
+
+closePopupBtn.addEventListener("click", function () {
+  popup.style.display = "none";
+});
+
+cards.forEach((card) => {
+  card.addEventListener("click", function (event) {
+    if (event.target.classList.contains("goTrash")) {
+      let newCard = card.cloneNode(true);
+      trashOut.append(newCard);
+    }
+  });
+});
